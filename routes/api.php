a31005a9e475ebb25149b38eae857c4cf0d5ed1b
Diego <diego.jaldim@gmail.com>
2020-08-05 19:49:31 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+$router->group(['prefix' => '/v1'], function () use ($router) {
+    $router->get('/', function () use ($router) {
+        return 'Api v1 on!';
+    });
+
+    $router->group(['prefix' => '/transaction'], function () use ($router) {
+        $router->get('/', function () use ($router) {
+            return 'Make a transaction';
+        });
+    });
+});
